9a120630fad306ff586adb802f376fc01d2b7db3
thanawat <tha_chookes@hotmail.com>
2017-07-12 11:27:02 +0700

    removed commment

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 
 import stylesheet from 'styles/index.scss'
-// or, if you work with plain css
-// import stylesheet from 'styles/index.css'
 
 export default () =>
   <div>
